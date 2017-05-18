4373776d5485c76783263da992c97bcdeb51f612
Mars-Dev <bcawosxy@gmail.com>
2017-05-18 18:33:53 +0800

    測試資料庫

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,16 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   <?php 
+                        try {
+                            DB::connection()->getPdo();
+                        } catch (\Exception $e) {
+                            die("Could not connect to the database.  Please check your configuration.");
+                        }
+                   ?> 
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
